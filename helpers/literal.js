ee1913a0450f8d4c553a8920bd45da2f8c564976
kitsune <aow.anti.hero@gmail.com>
2021-06-02 09:39:44 +0800

    backward compatibility

--- helpers/literal.js
+++ helpers/literal.js
@@ -8,7 +8,12 @@ function literal(template, ...strings) {
   let s = template;
   const str = [].slice.call(strings);
   for (let i = 0; i < str.length; i=i+2) {
-    s = s.replaceAll(str[i], str[i+1]);
+    if (typeof s.replaceAll == 'function') {
+      s = s.replaceAll(str[i], str[i+1]);
+    } else {
+      reg = new RegExp(str[i], 'g');
+      s = s.replace(reg, str[i+1]);
+    }
   }
   return s;
 }
