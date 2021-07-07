af79e91851fc0f13538a0579b00f0e2c3fd8c03d
kitsune <aow.anti.hero@gmail.com>
2021-07-07 10:38:21 +0800

    utc

---
+++ commands/events/utc.js
@@ -0,0 +1,30 @@
+const {formatDate} = require('./event');
+
+module.exports = {
+  name: 'utc',
+  async execute(cmdRes, settings, msg, args) {
+    const localTime = new Date();
+
+    const YYYY = localTime.getUTCFullYear();
+
+    let MM = localTime.getUTCMonth() + 1;
+    if (MM < 10) MM = '0' + MM;
+
+    let DD = localTime.getUTCDate();
+    if (DD < 10) DD = '0' + DD;
+
+    let hh = localTime.getUTCHours();
+    if (hh < 10) hh = '0' + hh;
+
+    let mm = localTime.getUTCMinutes();
+    if (mm < 10) mm = '0' + mm;
+
+    let ss = localTime.getUTCSeconds();
+    if (ss < 10) ss = '0' + ss;
+
+    let ms = localTime.getUTCMilliseconds();
+    if (ms < 10) ms = '0' + ms;
+
+    msg.channel.send(`${YYYY}-${MM}-${DD} ${hh}:${mm}:${ss}:${ms}`);
+  },
+};
