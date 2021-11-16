de6912adef126791eb4916a801e256b09eb7dbbb
kitsune <aow.anti.hero@gmail.com>
2021-11-16 10:11:05 +0800

    finishing up

--- commands/events/event-of.js
+++ commands/events/event-of.js
@@ -1,28 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 const res = require('../../res/res');
+const {literal} = require('../../helpers/literal');
 const {formatDate} = require('./event');
 
 module.exports = {
   name: 'event-of',
   async execute(cmdRes, settings, msg, args) {
+    const l10n = res.l10n[settings.lang];
     const FILE_PATH = path.resolve(__dirname, '../../events.json');
     const EVENTS = JSON.parse(fs.readFileSync(FILE_PATH));
 
+    const CM = '<:eventcard:909986289535832104>';
+    const WOF = '<:eventwheel:909986289552617502>';
+
     const hero = res.findHero(settings.lang, args[0]);
     if (!hero) return;
+
+    const heroDisplayName = l10n.HERO_DISPLAY_NAMES[hero];
+
+    if (hero == 'athena') {
+      msg.channel.send(literal(l10n.NO_INFO, '{TEXT}', heroDisplayName));
+      return;
+    }
 
     const results = [];
     for (let i=0; i < EVENTS.events.length; i++) {
       event = EVENTS.events[i];
       if (event.heroes.indexOf(hero) != -1) {
-        r = formatDate(new Date(event.date)) +
-            ((event.heroes.length > 2)?' (Card Master)':' (Wheel)');
-        results.push(r);
+        results.push(
+            ((event.heroes.length > 2)?CM:WOF) +
+            ' ' + formatDate(new Date(event.date)),
+        );
         if (results.length >= 5) break;
       };
     }
-    if (results.length == 0) return;
-    msg.channel.send(results.join('\n'));
+    if (results.length == 0) {
+      msg.channel.send(literal(l10n.NO_INFO, '{TEXT}', heroDisplayName));
+    } else {
+      msg.channel.send(results.join('\n'));
+    }
   },
 };
