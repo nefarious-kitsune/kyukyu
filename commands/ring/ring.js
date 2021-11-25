13a7159c0974f32d4c414205d7deb9c8c33e59f1
kitsune <aow.anti.hero@gmail.com>
2021-11-25 16:34:24 +0800

    Squashed commit of the following:
    commit 6ff8061889755d9274ddc72d17ebbb630fa90b0d
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Thu Nov 25 16:29:22 2021 +0800
    
        WIP
    
    commit d3f9c8311f10cc7280d933dae7d8331aa06a69fe
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Thu Nov 25 16:19:01 2021 +0800
    
        WIP
    
    commit dc4d54230b9649e6b41a9cc7d0853b6b514bb3b4
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Thu Nov 25 12:21:54 2021 +0800
    
        WIP
    
    commit 6ad5ab1c715f0fe4faebf1407ad395c45afaab83
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 16:31:35 2021 +0800
    
        WIP
    
    commit c45deee07494ff24d630e52748cba33e9be3de32
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 15:00:54 2021 +0800
    
        WIP
    
    commit 5ecb0e280295a6af5c91a3c555f00ba3c8fb19af
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 14:58:33 2021 +0800
    
        WIP
    
    commit 48beeafd367b2a9b8070865671a258bf3e4ad60b
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 12:55:50 2021 +0800
    
        WIP
    
    commit 231b347bede3d0a8976719398718e90d6a337f52
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 12:42:53 2021 +0800
    
        WIP
    
    commit 152281338802f61e9c6f60ab0a0d2af8520fe133
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 12:24:14 2021 +0800
    
        WIP
    
    commit de88fdb41caabbc612b73ebc10a830df81d1bc20
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 12:11:37 2021 +0800
    
        WIP
    
    commit e997312bfd0ff0e7d554bbe2660c86c0f3a5f19c
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 11:28:18 2021 +0800
    
        WIP
    
    commit e85e321133282f4e395e0ca0781cbfc8cf23a899
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 10:30:21 2021 +0800
    
        WIP
    
    commit 7e00548e3c337f63832d40b37a235ad7dd6db0cf
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 10:01:53 2021 +0800
    
        WIP
    
    commit aaae3046eb2db22d3cc73bb03e4064b7e0d4915f
    Author: kitsune <aow.anti.hero@gmail.com>
    Date:   Wed Nov 24 09:54:50 2021 +0800
    
        WIP

--- commands/ring/ring.js
+++ commands/ring/ring.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const {literal} = require('../../helpers/literal');
-const {pause} = require('./src/common');
+const {wait, pause} = require('./src/common');
 const L10N_EN = require('./en/_l10n');
-const ENROLL = require('./en/enroll');
+const Enroll = require('./src/enroll');
 const Master = require('./src/ringMaster');
 
 // const BLUE = 0x3170a6; // (49, 112, 166)
@@ -25,43 +25,52 @@ There was a heavy shower, and the road became very muddy.
 
 module.exports = {
   name: 'ring',
-  async start(msg) {
-    const allSettings = JSON.parse(
-        fs.readFileSync(path.resolve(__dirname, './ring.json')),
-    );
-    if (!allSettings.hasOwnProperty(msg.guildId)) return;
+  async start(gameSettings) {
+    const master = new Master(gameSettings, L10N_EN);
+    const enroll = new Enroll(gameSettings, master);
 
-    const guildSettings = allSettings[msg.guildId];
-    if (guildSettings.gameChannel != msg.channelId) return;
+    const preannouncement = literal(
+        L10N_EN.PREANNOUNCEMENT, '{SECONDS}', gameSettings.pauseBeforeGame);
+    const letsgo = literal(
+        L10N_EN.LETSGO, '{CHANNEL ID}', gameSettings.gameChannelId);
 
-    const generalChat =
-        await msg.guild.channels.fetch(guildSettings.generalChannel);
-
-    const master = new Master(
-        msg.author, msg.channel, guildSettings, L10N_EN);
-
-    const enroll = new ENROLL(guildSettings, master);
-
-    generalChat.send(literal(
-        L10N_EN.PREANNOUNCEMENT,
-        '{SECONDS}', guildSettings.pauseBeforeGame),
-    );
-
+    gameSettings.generalChannel.send(preannouncement);
+    // Preload enrollment message (buttons disabled)
     enroll.announce();
-
-    pause(guildSettings.pauseBeforeGame).then(() => {
-      generalChat.send(L10N_EN.LETSGO);
-      enroll.start();
-      pause(guildSettings.entryTimeLimit).then(()=> {
-        enroll.stop();
-        pause(guildSettings.pauseBeforeDay)
-            .then(() => master.startDay());
-      });
+    pause(gameSettings.pauseBeforeGame).then(()=>{
+      gameSettings.generalChannel.send(letsgo);
+      enroll.start(); // Enable buttons
     });
   },
   async execute(cmdRes, settings, msg, args) {
     if (msg.channel.type == 'DM') return;
-    this.start(msg);
+
+    const allSettings = JSON.parse(
+        fs.readFileSync(path.resolve(__dirname, './ring.json')),
+    );
+
+    // Exit if guild settings cannot be found
+    if (!allSettings.hasOwnProperty(msg.guildId)) return;
+
+    // Exit if the command is not executed in game channel
+    const guildSettings = allSettings[msg.guildId];
+    if (guildSettings.gameChannelId != msg.channelId) return;
+
+    // Exit if the command is not executed by an Fire Starter
+    if (!guildSettings.fireStarterIds.includes(msg.author.id)) return;
+
+    const guild = msg.guild;
+
+    const generalChannel =
+      await guild.channels.fetch(guildSettings.generalChannelId);
+    const role = await guild.roles.fetch(guildSettings.roleId);
+
+    const gameSettings = Object.assign({}, guildSettings);
+    gameSettings.generalChannel = generalChannel;
+    gameSettings.gameChannel = msg.channel;
+    gameSettings.roleName = role.name;
+    gameSettings.fireStarter = msg.author;
+    this.start(gameSettings);
   },
 };
 
---
+++ commands/ring/src/enroll.js
@@ -0,0 +1,178 @@
+const {literal} = require('../../../helpers/literal');
+const {pause} = require('./common');
+
+/* eslint-disable max-len */
+const RING_IMG_URL = 'https://cdn.discordapp.com/attachments/833978786395586600/903096090977509446/the-ring.png';
+/* eslint-enable max-len */
+
+const REFRESH_INTERVAL = 3;
+
+/** Enrollment manager */
+class Enrollment {
+  /**
+   * Constructor
+   * @param {object} gameSettings game settings
+   * @param {Master} master Ring Master
+   * */
+  constructor(gameSettings, master) {
+    this.reset();
+    this.l10n = master.l10n.ENROLL;
+    this.master = master;
+    this.gameSettings = gameSettings;
+    this.headStartText = '';
+  }
+
+  /** Reset everything */
+  reset() {
+    this.master = null;
+    this.gameSettings = null;
+    this.L10N = null;
+    this.refresher = null;
+    this.annoucementMessage = null;
+    this.enrollmentCollector = null;
+    this.countDown = 0;
+    this.updateBoard = null;
+    this.contestantIds = [];
+  }
+
+  /** Make an annoucement */
+  async announce() {
+    this.announcementText = literal(
+        this.l10n.ANNOUNCEMENT_MSG,
+        '{PLAYER LIMIT}', this.gameSettings.playerLimit,
+        '{WINNER LIMIT}', this.gameSettings.winnerLimit,
+        '{ROLE NAME}', this.gameSettings.roleName,
+    );
+
+    const content = {
+      embeds: [{
+        thumbnail: {url: RING_IMG_URL},
+        description: this.announcementText,
+        color: 0x3170a6,
+      }],
+      components: [{
+        type: 1,
+        components: [{
+          type: 2, label: this.l10n.JOIN,
+          style: 1, custom_id: 'join', disabled: true,
+        }],
+      }],
+    };
+
+    const annoucement = await this.gameSettings.gameChannel.send(content);
+
+    this.annoucementMessage = annoucement;
+    annoucement.channel
+        .send(this.l10n.STARTING_SOON)
+        .then((startingSoon)=>{
+          this.updateBoard = startingSoon;
+          this.master.updateBoard = startingSoon;
+        });
+  }
+
+  /** Start enrolling players */
+  start() {
+    this.enrollmentCollector =
+      this.annoucementMessage.createMessageComponentCollector(
+          {time: this.gameSettings.entryTimeLimit * 1000},
+      );
+
+    this.enrollmentCollector.on('collect', (i) => {
+      this.onCollect(i);
+    });
+
+    this.enrollmentCollector.on('end', (i, reason) => {
+      if (reason == 'time') clearInterval(this.refresher);
+      this.onEnd(i);
+    });
+
+    this.annoucementMessage.edit({
+      embeds: [{
+        thumbnail: {url: RING_IMG_URL},
+        description: this.announcementText,
+        color: 0x3170a6,
+      }],
+      components: [{
+        type: 1,
+        components: [{
+          type: 2, label: this.l10n.JOIN,
+          style: 1, custom_id: 'join',
+        }],
+      }],
+    });
+    this.countDown = this.gameSettings.entryTimeLimit;
+    this.refresh();
+    this.refresher = setInterval(
+        () => this.refresh(),
+        REFRESH_INTERVAL * 1000);
+  }
+
+  /**
+   * Process interaction
+   * @param {Interaction} i Interaction
+   */
+  onCollect(i) {
+    if (this.contestantIds.indexOf(i.member.id) != -1) {
+      i.deferUpdate();
+      return;
+    }
+    console.log('user collected');
+    i.reply({content: this.l10n.WELCOME, ephemeral: true});
+    const newPlayer = this.master.addPlayer(i);
+    this.master.log(`${newPlayer.playerName} has joined the RiNGs.`);
+
+    this.contestantIds.push(i.member.id);
+    if (this.contestantIds.length ==3) {
+      this.master.players[0].medal = 1;
+      this.master.players[1].medal = 1;
+      this.master.players[2].medal = 1;
+      this.headStartText = literal(
+          this.l10n.HEAD_START,
+          '{PLAYER 1}', this.master.players[0].playerName,
+          '{PLAYER 2}', this.master.players[1].playerName,
+          '{PLAYER 3}', this.master.players[2].playerName,
+      );
+    }
+    if (this.contestantIds.length >= this.gameSettings.playerLimit) {
+      this.enrollmentCollector.stop();
+    }
+  }
+
+  /** Countdown update */
+  refresh() {
+    if (this.countDown <= 0) {
+      this.countDown = 0;
+      clearInterval(this.refresher);
+    }
+    this.updateBoard.edit(
+        this.headStartText +
+        literal(this.l10n.TIME_REMAINING, '{SECONDS}', this.countDown),
+    );
+    this.countDown -= REFRESH_INTERVAL;
+  }
+
+  /** Countdown stop */
+  async onEnd() {
+    console.log('enrollment has ended');
+    this.annoucementMessage.edit({
+      embeds: [{
+        title: 'RiNGs',
+        thumbnail: {url: RING_IMG_URL},
+        description: this.announcementText,
+        color: 0x3170a6,
+      }],
+      components: [],
+    });
+    this.updateBoard.edit(
+        this.headStartText +
+        literal(
+            this.l10n.STARTED,
+            '{PLAYER COUNT}', this.contestantIds.length),
+    );
+    this.master.startDay();
+  }
+};
+
+/* eslint-disable max-len */
+module.exports = Enrollment;
+
